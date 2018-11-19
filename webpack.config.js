dc1ae79008b113c10c785a03eb0d9dbeefc6c600
Jiří Čermák <jiri.cermak@ackee.cz>
2018-11-19 15:51:22 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'ReduxTokenAuth',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            '@babel/plugin-proposal-object-rest-spread',
+                            '@babel/plugin-proposal-export-namespace-from',
+                            '@babel/plugin-transform-runtime',
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [],
+    mode: 'development',
+};
