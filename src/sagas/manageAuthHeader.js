ec2b4a559aa7c4cc54fbf83c1389ca7d6b645172
Jiří Čermák <jiri.cermak@ackee.cz>
2019-01-28 10:01:42 +0100

    Fix @ackee/petrus imports

--- src/sagas/manageAuthHeader.js
+++ src/sagas/manageAuthHeader.js
@@ -1,5 +1,5 @@
 import { takeEvery } from 'redux-saga/effects';
-import { actionTypes } from '@ackee/petrus';
+import { actionTypes } from '@ackee/petrus/worker';
 
 import * as Store from '../store';
 
