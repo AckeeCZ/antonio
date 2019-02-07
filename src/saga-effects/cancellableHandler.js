372e36cd041ae12fa69edd9fed4e1085197da340
Jiří Čermák <jiri.cermak@ackee.cz>
2019-02-07 13:30:04 +0100

    Fix cancellableHandler utility

--- src/saga-effects/cancellableHandler.js
+++ src/saga-effects/cancellableHandler.js
@@ -1,18 +1,22 @@
 import { CancelToken } from 'axios';
 import { call, take, race } from 'redux-saga/effects';
 
+const noop = () => {};
+
 /**
  *
  * @param {{handlerArg: Any, CANCEL: String, handler: Function, onComplete: Function?}}
  */
-export default function* cancellableHandler({ handlerArg, CANCEL, handler, onComplete }) {
+export default function* cancellableHandler({ handlerArg, CANCEL, handler, onComplete = noop }) {
     const source = CancelToken.source();
 
+    function* tasks() {
+        yield call(handler, handlerArg, source.token);
+        yield call(onComplete);
+    }
+
     const result = yield race({
-        *handler() {
-            yield call(handler, handlerArg, source.token);
-            yield call(onComplete);
-        },
+        tasks: call(tasks),
         cancel: take(CANCEL),
     });
 
--- src/saga-effects/effects/takeRequest.js
+++ src/saga-effects/effects/takeRequest.js
@@ -4,15 +4,17 @@ import cancellableHandler from '../cancellableHandler';
 /**
  * Blocking custom saga effect that can actually cancel the API request
  * @param {{ REQUEST: String, CANCEL: String }} params
- * @param {Function} task
+ * @param {Function} handler
  */
-export default function* takeRequest(actionTypes = {}, task) {
+export default function* takeRequest(actionTypes = {}, handler) {
     while (true) {
         const action = yield take(actionTypes.REQUEST);
 
+        console.log(action);
+
         yield cancellableHandler({
-            task,
-            taskArg: action,
+            handler,
+            handlerArg: action,
             CANCEL: actionTypes.CANCEL,
         });
     }
