9e658782913421311bf197e2b48414d1c4b942cb
Jiří Čermák <jiri.cermak@ackee.cz>
2019-11-15 12:36:43 +0100

    Implement @babel/preset-modules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 3.0.0-beta.7 - 2019-11-15
+
+### Update
+
+-   Upgrade dependencies.
+-   Update babel config:
+    -   Use `@babel/preset-modules` -> decreased package size.
+    -   Enable `loose` option -> decreased package size.
+
 ## 3.0.0-beta.6 - 2019-10-22
 
 ### Updated
--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,8 @@
 const path = require('path');
 
-module.exports = {
-    presets: [
-        [
-            '@babel/env',
-            {
-                modules: process.env.BABEL_ENV === 'es' ? false : 'auto',
-            },
-        ],
-    ],
+const config = {
+    presets: [],
     plugins: [
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-transform-runtime',
         [
             'babel-plugin-custom-import-path-transform',
             {
@@ -19,5 +10,25 @@ module.exports = {
             },
         ],
     ],
-    ignore: ['**/__tests__/'],
+    ignore: ['**/__tests__/', '**/*.test.js'],
 };
+
+if (process.env.BABEL_ENV === 'es') {
+    config.presets.push([
+        '@babel/modules',
+        {
+            loose: true,
+        },
+    ]);
+} else {
+    config.presets.push([
+        '@babel/env',
+        {
+            loose: true,
+        },
+    ]);
+
+    config.plugins.push(...['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime']);
+}
+
+module.exports = config;
