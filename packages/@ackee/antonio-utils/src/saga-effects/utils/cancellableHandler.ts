149a31a75531c1b6e96a27782c5d90760f11070c
Jiří Čermák <jiri.cermak@ackee.cz>
2021-05-05 14:24:12 +0200

    :bug: Fix adding AbortController polyfill

--- packages/@ackee/antonio-utils/src/saga-effects/utils/cancellableHandler.ts
+++ packages/@ackee/antonio-utils/src/saga-effects/utils/cancellableHandler.ts
@@ -1,11 +1,12 @@
-import AbortControllerPolyfill from 'abortcontroller-polyfill';
+import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only.js';
+
 import { call, take, race } from 'redux-saga/effects';
 import { CancellableHandler } from 'types';
 
 const noop = function* () {};
 
 export default function* cancellableHandler({ handlerArg, CANCEL, handler, onComplete = noop }: CancellableHandler) {
-    const controller: AbortController = new AbortControllerPolyfill();
+    const controller = new AbortController();
 
     function* tasks() {
         yield call(handler, handlerArg, controller.signal);
