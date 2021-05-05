5069fa2415788abc577fe653e300da53e475faac
Jiří Čermák <jiri.cermak@ackee.cz>
2021-05-05 13:29:53 +0200

    :sparkles: Add polyfill for AbortController

--- packages/@ackee/antonio-utils/src/saga-effects/utils/cancellableHandler.ts
+++ packages/@ackee/antonio-utils/src/saga-effects/utils/cancellableHandler.ts
@@ -1,12 +1,11 @@
+import AbortControllerPolyfill from 'abortcontroller-polyfill';
 import { call, take, race } from 'redux-saga/effects';
 import { CancellableHandler } from 'types';
 
 const noop = function* () {};
 
 export default function* cancellableHandler({ handlerArg, CANCEL, handler, onComplete = noop }: CancellableHandler) {
-    // FIXME: add polyfill
-    // eslint-disable-next-line compat/compat
-    const controller = new AbortController();
+    const controller: AbortController = new AbortControllerPolyfill();
 
     function* tasks() {
         yield call(handler, handlerArg, controller.signal);
