5aa35969fbf8597a2d6daee09e432e94e5501ab0
Jiří Čermák <jiri.cermak@ackee.cz>
2021-05-06 12:37:10 +0200

    :bug: Fix default token type at setAuthHeader

--- packages/@ackee/antonio-utils/src/setAuthHeader.ts
+++ packages/@ackee/antonio-utils/src/setAuthHeader.ts
@@ -1,11 +1,11 @@
-enum TokenType {
-    BEARER = 'Bearer',
-}
+const TokenType = {
+    BEARER: 'Bearer',
+};
 
 export default function setAuthHeader(
     headers: Headers,
     tokenValue?: string,
-    tokenType: TokenType | string = TokenType.BEARER,
+    tokenType: string = TokenType.BEARER,
 ): void {
     if (tokenValue) {
         headers.set('Authorization', `${tokenType} ${tokenValue}`);
