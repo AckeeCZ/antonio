01aea0facc844671487cf067e16f9377e2c86c99
Jiří Čermák <jiri.cermak@ackee.cz>
2021-05-21 10:19:23 +0200

    :wrench: Adjust babel config (remove plugins that are included the @babel/preset-env)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
-const path = require('path');
-
-const config = {
+module.exports = {
     presets: [
-        '@babel/typescript',
+        [
+            '@babel/typescript',
+            {
+                onlyRemoveTypeImports: true,
+                allowDeclareFields: true,
+            },
+        ],
         [
             '@babel/env',
             {
@@ -15,34 +19,5 @@ const config = {
             },
         ],
     ],
-    plugins: [
-        [
-            '@babel/proposal-class-properties',
-            {
-                loose: true,
-            },
-        ],
-        '@babel/proposal-object-rest-spread',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        [
-            'babel-plugin-custom-import-path-transform',
-            {
-                transformImportPath: path.resolve(__dirname, 'scripts/transformImportPath.js'),
-            },
-        ],
-        [
-            'babel-plugin-transform-imports',
-            {
-                lodash: {
-                    transform: 'lodash/${member}',
-                    preventFullImport: true,
-                },
-            },
-        ],
-        '@babel/plugin-transform-runtime',
-    ],
     ignore: ['**/__tests__/', '**/*.test.js'],
 };
-
-module.exports = config;
