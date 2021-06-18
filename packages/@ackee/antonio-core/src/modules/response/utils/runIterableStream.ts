05f69c2cd8955509609bddd8bc09eb6e0bd7a1e2
Jiří Čermák <jiri.cermak@ackee.cz>
2021-06-18 15:15:00 +0200

    :children_crossing: runIterableStream: throw an type error if anything other than async iterator has been passed in

--- packages/@ackee/antonio-core/src/modules/response/utils/runIterableStream.ts
+++ packages/@ackee/antonio-core/src/modules/response/utils/runIterableStream.ts
@@ -6,6 +6,12 @@ export function* runIterableStream<T = string | any[]>(
     it: AsyncGenerator<T, void, unknown>,
     onProgress: OnProgress<T>,
 ) {
+    if (!it[Symbol.asyncIterator]) {
+        throw new TypeError(
+            `The 1st arg must be async generator. Check you have set the 'responseDataType' to 'iterableStream'.`,
+        );
+    }
+
     while (true) {
         const { done, value } = yield it.next();
 
