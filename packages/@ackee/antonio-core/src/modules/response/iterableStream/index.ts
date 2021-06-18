7eacf541eecb29092951b433f56093146b2bf5de
Jiří Čermák <jiri.cermak@ackee.cz>
2021-06-18 14:24:39 +0200

    :bug: Fix iterableStream - resolve the generator even the stream turns out to be empty

--- packages/@ackee/antonio-core/src/modules/response/iterableStream/index.ts
+++ packages/@ackee/antonio-core/src/modules/response/iterableStream/index.ts
@@ -6,23 +6,32 @@ export async function* iterableStream(stream: Body['body'], contentTypeIsJson: b
     }
 
     const reader = stream.getReader();
-    const decoder = new TextDecoder();
-    const parser = contentTypeIsJson ? new ChunkToJsonParser() : null;
 
     try {
+        const decoder = new TextDecoder();
+        const parser = contentTypeIsJson ? new ChunkToJsonParser() : null;
         let result = await reader.read();
+        let yieldedSome = false;
+        let parsedChunk: string | any[] | undefined;
 
         while (!result.done) {
             const arrayBuffer = result.value;
             const decodedChunk = decoder.decode(arrayBuffer, { stream: true });
 
-            const parsedChunk = parser ? parser.parse(decodedChunk) : decodedChunk;
+            parsedChunk = parser ? parser.parse(decodedChunk) : decodedChunk;
 
             if (parsedChunk.length > 0) {
                 yield parsedChunk;
+                yieldedSome = true;
             }
 
             result = await reader.read();
+        }
+
+        // If every chunk was empty, yield now, at the end, the empty chunk as the last value.
+        // Otherwise the generator wouldn't ever end.
+        if (!yieldedSome) {
+            yield parsedChunk;
         }
     } catch (e) {
         reader.releaseLock();
