7b9fced71e75568b589408f0192ef6134da7d40a
Jiří Čermák <jiri.cermak@ackee.cz>
2021-06-21 17:23:10 +0200

    :white_check_mark: Add test for each req. method

--- packages/@ackee/antonio-core/src/modules/core/models/__tests__/index.ts
+++ packages/@ackee/antonio-core/src/modules/core/models/__tests__/index.ts
@@ -1,13 +1,27 @@
 import 'whatwg-fetch';
+import Headers from 'fetch-headers';
 import { generatorToPromise as toPromise } from '../../makeRequest';
 import { Antonio } from '../Antonio';
 
-describe('Antonio class', () => {
+interface User {
+    id: string;
+}
+
+type Users = User[];
+
+/**
+    README:
+        The API never actually update the database and returns stale data. 
+        That's why no test below checks if the returned data contains data from a request.
+*/
+
+describe('Antonio', () => {
     let api: Antonio;
+    const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 
     beforeEach(() => {
         api = new Antonio({
-            baseURL: 'https://jsonplaceholder.typicode.com/',
+            baseURL: BASE_URL,
         });
     });
 
@@ -15,24 +29,89 @@ describe('Antonio class', () => {
         api.destroy();
     });
 
-    it('fetch users', async () => {
-        const { data, response, request } = await toPromise(api.get('/users'));
+    it('has correct default config', () => {
+        expect(api.defaults).toEqual({
+            baseURL: BASE_URL,
+            headers: new Headers(),
+        });
+    });
+
+    it('has interceptors interface setup', () => {
+        expect(api.interceptors).toHaveProperty('request');
+        expect(api.interceptors).toHaveProperty('response');
+    });
+
+    it('fetches users', async () => {
+        const { data, response, request } = await toPromise(api.get<Users>('/users'));
 
         expect(data).toBeInstanceOf(Array);
         expect(response).toBeInstanceOf(Response);
         expect(request).toBeInstanceOf(Request);
     });
 
-    // TODO:
-    it.skip('create a user', async () => {
-        interface User {
-            name: string;
-        }
-        const user: User = {
-            name: 'Alois',
-        };
-        const { data } = await toPromise(api.post('/users', user));
+    it('deletes an user', async () => {
+        await toPromise(
+            api.delete('/users/:id', {
+                uriParams: { id: 1 },
+            }),
+        );
+    });
 
-        expect(data).toContain(user);
+    it('updates an user', async () => {
+        const { data } = await toPromise(
+            api.put<User>(
+                '/users/:id',
+                { user: 'Alois' },
+                {
+                    uriParams: { id: 1 },
+                },
+            ),
+        );
+
+        expect(data).toEqual({ id: 1 });
+    });
+
+    it('fetches an user', async () => {
+        const { data } = await toPromise(
+            api.get<User>('/users/:id', {
+                uriParams: { id: 1 },
+            }),
+        );
+
+        expect(data).toHaveProperty('id');
+        expect(data.id).toBe(1);
+    });
+
+    it('does an patch update of an user', async () => {
+        const { data } = await toPromise(
+            api.get<User>('/users/:id', {
+                uriParams: { id: 1 },
+            }),
+        );
+
+        expect(data).toHaveProperty('id');
+        expect(data.id).toBe(1);
+    });
+
+    it('creates a user', async () => {
+        const { data } = await toPromise(
+            api.post<User>('/users', {
+                name: 'Alois',
+            }),
+        );
+
+        expect(data).toHaveProperty('id');
+    });
+
+    it('makes an HEAD req.', async () => {
+        const result = await toPromise(api.head('/users'));
+
+        expect(result.data).toBe(null);
+    });
+
+    it('makes an OPTIONS req.', async () => {
+        const result = await toPromise(api.options('/users'));
+
+        expect(result.data).toBe(null);
     });
 });
