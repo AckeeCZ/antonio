9babdd0724a21d16483fae66d10565d2d11dcc78
Jiří Čermák <jiri.cermak@ackee.cz>
2021-06-28 15:34:08 +0200

    :sparkles: Add possible to set Antonio class level generics as defaults of TSuccessData & TErrorData

--- packages/@ackee/antonio-core/src/modules/core/models/Antonio.ts
+++ packages/@ackee/antonio-core/src/modules/core/models/Antonio.ts
@@ -3,14 +3,15 @@ import { RequestConfig, RequestBodyData } from '../../../types';
 import { mergeRequestConfigs } from '../../request/utils';
 import { defaultRequestConfig, DefaultRequestConfig } from '../../request/config';
 
-import { defaultGeneralConfig, generalConfigs } from '../general-config';
+import { defaultGeneralConfig } from '../general-config';
 import type { GeneralConfig } from '../general-config';
 import makeRequest from '../makeRequest';
 
 import InterceptorManager, { InterceptorManagers, interceptors } from '../../interceptors/InterceptorManager';
-export class Antonio {
+export class Antonio<TSuccessDataDefault = any, TErrorDataDefault = any> {
     readonly defaults: DefaultRequestConfig;
     readonly interceptors: InterceptorManagers;
+    readonly generalConfig: GeneralConfig;
 
     /**
      * @example
@@ -30,40 +31,61 @@ export class Antonio {
             response: new InterceptorManager<Response, RequestConfig>(),
         });
 
-        generalConfigs.set(
-            this,
-            Object.freeze<GeneralConfig>({
-                ...defaultGeneralConfig,
-                ...generalConfig,
-            }),
-        );
+        this.generalConfig = Object.freeze<GeneralConfig>({
+            ...defaultGeneralConfig,
+            ...generalConfig,
+        });
     }
 
-    post<TSuccessData = any, TErrorData = any>(url: string, body: RequestBodyData, requestConfig?: RequestConfig) {
+    post<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        body: RequestBodyData,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('POST', url, body, requestConfig, this);
     }
 
-    put<TSuccessData = any, TErrorData = any>(url: string, body: RequestBodyData, requestConfig?: RequestConfig) {
+    put<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        body: RequestBodyData,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('PUT', url, body, requestConfig, this);
     }
 
-    patch<TSuccessData = any, TErrorData = any>(url: string, body: RequestBodyData, requestConfig?: RequestConfig) {
+    patch<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        body: RequestBodyData,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('PATCH', url, body, requestConfig, this);
     }
 
-    get<TSuccessData = any, TErrorData = any>(url: string, requestConfig?: RequestConfig) {
+    get<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('GET', url, undefined, requestConfig, this);
     }
 
-    delete<TSuccessData = any, TErrorData = any>(url: string, requestConfig?: RequestConfig) {
+    delete<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('DELETE', url, undefined, requestConfig, this);
     }
 
-    head<TSuccessData = any, TErrorData = any>(url: string, requestConfig?: RequestConfig) {
+    head<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('HEAD', url, undefined, requestConfig, this);
     }
 
-    options<TSuccessData = any, TErrorData = any>(url: string, requestConfig?: RequestConfig) {
+    options<TSuccessData = TSuccessDataDefault, TErrorData = TErrorDataDefault>(
+        url: string,
+        requestConfig?: RequestConfig,
+    ) {
         return makeRequest<TSuccessData, TErrorData>('OPTIONS', url, undefined, requestConfig, this);
     }
 
@@ -72,7 +94,6 @@ export class Antonio {
      */
     destroy() {
         interceptors.delete(this);
-        generalConfigs.delete(this);
     }
 }
 
