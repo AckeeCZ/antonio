a9b55bd0e6dadbc3b66bb303be5968c5a8403132
Jiří Čermák <jiri.cermak@ackee.cz>
2021-08-04 12:50:10 +0200

    :boom: Don't set default mime type in request headers

--- packages/@ackee/antonio-core/src/modules/request/utils/request.ts
+++ packages/@ackee/antonio-core/src/modules/request/utils/request.ts
@@ -1,32 +1,16 @@
-import type { RequestMethod, RequestConfig, RequestHeaders, RequestParams } from '../../../types';
-import { Header } from '../../../constants';
+import type { RequestMethod, RequestParams } from '../../../types';
 import type { GeneralConfig } from '../../core/general-config';
-
-import { getDefaultRequestMimeType } from '../mimeTypes';
 
 import { createRequestUrl } from './url';
 import { formatBodyData } from './formatBodyData';
-
-function setContentTypeHeader(method: RequestMethod, config: RequestConfig, body?: BodyInit): RequestHeaders {
-    const headers = config.headers as Headers;
-
-    if (!headers.has(Header.CONTENT_TYPE) && method !== 'HEAD' && body) {
-        const defaultMimeType = getDefaultRequestMimeType(body);
-        if (defaultMimeType) {
-            headers.set(Header.CONTENT_TYPE, defaultMimeType);
-        }
-    }
-
-    return headers;
-}
 
 export function createRequestInit(method: RequestMethod, requestParams: RequestParams, generalConfig: GeneralConfig) {
     const { url, config, bodyData } = requestParams;
     const requestUrl = createRequestUrl(url, config, generalConfig);
 
-    const { mode, credentials, cache, redirect, referrer, referrerPolicy, integrity, keepalive, signal } = config;
+    const { mode, credentials, cache, redirect, referrer, referrerPolicy, integrity, keepalive, signal, headers } =
+        config;
     const body = formatBodyData(bodyData);
-    const headers = setContentTypeHeader(method, config, body);
     const requestInit: RequestInit = {
         method,
         mode,
