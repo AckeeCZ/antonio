eb77dfc0a7fcd81b5968922c7f9b0686f0945165
Jiří Čermák <jiri.cermak@ackee.cz>
2021-09-07 13:51:59 +0200

    :fire: Remove resolver types -> methods return sync generator by default (never promise)

--- packages/@ackee/antonio-core/src/modules/core/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const resolverTypes = {
-    GENERATOR: 'generator',
-    PROMISE: 'promise',
-} as const;
-
-type ResolverTypesKeys = keyof typeof resolverTypes;
-export type ResolverType = typeof resolverTypes[ResolverTypesKeys];
--- packages/@ackee/antonio-core/src/modules/core/index.ts
+++ packages/@ackee/antonio-core/src/modules/core/index.ts
@@ -1,3 +1,2 @@
-export { resolverTypes } from './constants';
 export { Antonio } from './models/Antonio';
 export { generatorToPromise } from './makeRequest';
