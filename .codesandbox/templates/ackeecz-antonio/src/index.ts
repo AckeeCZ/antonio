d9dd042c51100dc4a3a98a307ad319b8f470992f
Jiří Čermák <jiri.cermak@ackee.cz>
2021-09-10 15:35:52 +0200

    :recycle: Extend codesandbox template

--- .codesandbox/templates/ackeecz-antonio/src/index.ts
+++ .codesandbox/templates/ackeecz-antonio/src/index.ts
@@ -1,7 +1,13 @@
-import { Antonio } from '@ackee/antonio-core';
+import { Antonio, generatorToPromise } from '@ackee/antonio-core';
 
 const api = new Antonio({
     baseURL: 'https://jsonplaceholder.typicode.com/',
 });
 
-console.log(api);
+function* task() {
+    const res = yield* api.get('/todos');
+
+    console.log(res.data);
+}
+
+generatorToPromise(task());
